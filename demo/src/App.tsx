323d77197d44f5b60564d94d4be4ff0a8befc351
VitalijsF <vitalijs.fescenko@gmail.com>
2023-09-20 14:44:51 +0300

    udpate web demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -18,8 +18,8 @@ const ort = require("onnxruntime-web");
 import npyjs from "npyjs";
 
 // Define image, embedding and model paths
-const IMAGE_PATH = "/assets/data/dogs.jpg";
-const IMAGE_EMBEDDING = "/assets/data/dogs_embedding.npy";
+const IMAGE_PATH = "/assets/data/truck.jpg";
+const IMAGE_EMBEDDING = "/assets/data/truck_embedding.npy";
 const MODEL_DIR = "/model/sam_onnx_quantized_example.onnx";
 
 const App = () => {
